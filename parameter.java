bcbf5a7e9d4c0cdcb311e945e5209e687da566be
ZjuWxy <132632587+ZjuWxy@users.noreply.github.com>
2023-05-05 10:30:29 +0800

    Add files via upload

---
+++ parameter.java
@@ -0,0 +1,12 @@
+
+
+// 样本参数
+public class entry {
+    int index;             // 索引号
+    double RT;             // 保留时间
+    double m_z;            // 质荷比
+    double MassDefect;     // 质量亏损
+    double TIC;            // 峰强度
+    int NumMerged;         // 合并次数
+    int Quality;           // 信噪比
+}
